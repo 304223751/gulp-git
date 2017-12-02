e083929d8b712aa268a5a126b1a88c9a18f76f05
304223751 <18738056105@163.com>
2017-12-02 11:47:06 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+var run = require('run-sequence').use(gulp);
+gulp.task('init', function () {
+    git.init(function (err) {
+        if (err) throw err;
+    });
+});
+gulp.task('add', function () {
+    return gulp.src('./')
+        .pipe(git.add());
+});
+gulp.task('commit', function () {
+    return gulp.src('./')
+        .pipe(git.commit('initial commit'));
+});
+gulp.task('addremote', function () {
+    git.addRemote('origin', 'https://github.com/304223751/gulp-git.git', function (err) {
+        if (err) throw err;
+    });
+});
+gulp.task('push', function () {
+    git.push('origin', 'master', function (err) {
+        if (err) throw err;
+    });
+});
+gulp.task('default', function () {
+    run('init', 'add', 'commit', 'addremote', 'push');
+});
